ba44a33fa9588cd37c53d4a117598bafb423deab
Casssie <cassieareff@gmail.com>
2019-06-20 16:10:06 -0400

    complete itteration 3

--- Emojional/ViewController.swift
+++ Emojional/ViewController.swift
@@ -10,14 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let emojis = ["♥️": "heart", "🥰": "heart face", "🤰🏽": "pregnant", "😋": "goof", "🌸": "flower"]
+    let emojis = ["♥️": ["i heart you", "i love you", "love ya"], "🥰": ["cute", "pretty", "awww"], "🤰🏽": ["ohana means family 💖", "who is the father?", "look at my belly"], "😋": ["goofy", "heyyo", "wacky"], "🌸": ["flower", "pink", "bloom"]]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     @IBAction func showMessage(sender: UIButton) {
         var emoji = sender.titleLabel?.text
-        let alertController = UIAlertController(title: "Heyyo", message: emojis[emoji!], preferredStyle: .alert)
+        let alertController = UIAlertController(title: "Heyyo", message: emojis[emoji!]?.randomElement(), preferredStyle: .alert)
         alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
         present(alertController, animated: true, completion: nil)
 
